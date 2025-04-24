fd457d3b0ec8b4968cb282d15bf0c988952a2e48
SDONGHUAOFFICIAL <shanuksetman@gmail.com>
2025-04-25 00:37:37 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,13 +1,13 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'SDONGHUA',
   author: 'Bot',
-  botName: "SD BUG BOT V2.1",
+  botName: "SDONGHUA BOT V2.1",
   botOwner: 'SDONGHUA', // Your name
   ownerNumber: '94782886991', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
-  version: "1.0.0",
+  version: "2.1.0",
 };
 
 module.exports = settings;
